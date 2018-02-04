eca35d3a879cda9644d5dd0cb54bdf62506d02fb
Avinash Seethalam <sreethalam.avinash@gmail.com>
2018-02-04 09:13:50 -0800

    Hive UDF example

---
+++ hiveUDF/src/hiveUDF/ToUpper.java
@@ -0,0 +1,13 @@
+package hiveUDF;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.Text;
+
+public class ToUpper extends UDF {
+	  
+	  public Text evaluate(Text input) {
+	    if(input == null) return null;
+	    return new Text(input.toString().toUpperCase());
+	  }
+
+}
